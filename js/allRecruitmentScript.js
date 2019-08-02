a7af5dada3279ecc9c05ff5966e9baa9e92ffd46
aguy <alexandre.guy@4sh.fr>
2019-08-02 18:00:42 +0200

    Deploy new website 3.0.0

--- js/allRecruitmentScript.js
+++
@@ -1,9 +0,0 @@
-// Add here all script
-//@prepros-append lib/jquery-3.1.1.min.js
-//@prepros-append lib/cbp-scroller/classie.js
-//@prepros-append lib/cbp-scroller/cbpScroller.js
-//@prepros-append lib/alertifyjs/alertify.min.js
-//@prepros-append lib/progressive-image.js
-//@prepros-append menu-recruitment.js
-//@prepros-append smooth-scrool.js
-//@prepros-append contact.js
